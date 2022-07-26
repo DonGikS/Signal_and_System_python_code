d54131b598e6018ee2f24c7823963ee7198fb470
dongiks <ikspurt0505@gmail.com>
2022-07-26 22:16:06 +0900

    removed pandas module and added sineFunction in main

--- Code/main.py
+++ Code/main.py
@@ -1,10 +1,11 @@
 import matplotlib.pyplot as plt
 import unitStepFunction as uSF
 import unitRampFunction as uRF
-
+import sineFunction as sF
 def main():
     uSF.unitStepFunction()
     uRF.unitRampFunction()
+    sF.sineFunction()
     plt.tight_layout()
     plt.show()
 
