5a0f5bf72f58a58fac9b5f3db0438036bf2100c0
dongiks <ikspurt0505@gmail.com>
2022-08-03 17:45:21 +0900

    Added graph title and result image

--- Code/Example3/ODE.py
+++ Code/Example3/ODE.py
@@ -11,4 +11,4 @@ deq = Eq( y(t).diff(t,2)+2*y(t).diff(t,1)+17*y(t), 0 )
 result = dsolve( deq, ics= {y(0):2, y(t).diff(t).subs(t,0):2.6} )
 print(result)
 
-plot(result.rhs, xlim=(-10,0))
+plot(result.rhs,title = "Eq(y(t), (1.15*sin(4*t) + 2.0*cos(4*t))*exp(-t))", xlim=(-10,0))
