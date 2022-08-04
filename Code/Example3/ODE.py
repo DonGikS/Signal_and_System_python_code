3fde4bb7fbf863cab515ff692632185cb5b3964d
dongiks <ikspurt0505@gmail.com>
2022-08-04 09:26:22 +0900

    ODE Example3 graph scale downsizing and added Image

--- Code/Example3/ODE.py
+++ Code/Example3/ODE.py
@@ -1,14 +1,18 @@
 from sympy import *
 
-#variable
-t = symbols('t')
-#dependent variable
-y = symbols('y', cls=Function)
+def ODE_1():
+    #variable
+    t = symbols('t')
+    #dependent variable
+    y = symbols('y', cls=Function)
 
-#Ordinary Differential Equation
-deq = Eq( y(t).diff(t,2)+2*y(t).diff(t,1)+17*y(t), 0 )
+    #Ordinary Differential Equation
+    deq = Eq( y(t).diff(t,2)+2*y(t).diff(t,1)+17*y(t), 0 )
 
-result = dsolve( deq, ics= {y(0):2, y(t).diff(t).subs(t,0):2.6} )
-print(result)
+    result = dsolve( deq, ics= {y(0):2, y(t).diff(t,1).subs(t,0):2.6} )
+    print(result)
 
-plot(result.rhs,title = "Eq(y(t), (1.15*sin(4*t) + 2.0*cos(4*t))*exp(-t))", xlim=(-10,0))
+    plot(result.rhs,title = "Eq(y(t), (1.15*sin(4*t) + 2.0*cos(4*t))*exp(-t))", xlim=(-10,10),ylim=(-10,10))
+
+
+ODE_1()
